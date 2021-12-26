0d4ce35cf54427bed18cf130f0fca465fb1cf472
Leonid Tkachenko <leon24rus@gmail.com>
2021-12-26 08:28:47 +0700

    Rename global constants Add argv as input values Add generated gitignore

--- main.py
+++ main.py
@@ -7,22 +7,22 @@ from getpass import getpass
 
 # SSH host url
 # I dont need "ssh://"" and "port" because they are setted in config (c:\users\user\.ssh\config)
-HOST="192.168.100.4"
+gHostAddress="192.168.100.4"
 
 # Remote environment variable contains path to folder where git repos are will be created.
-REMOTE_GIT_ENV = "GIT_BARE_REPOS"
+gRemoteGitEnv = "GIT_BARE_REPOS"
 
 # Local environment variable contains path to folder where clone repos are will be cloned to.
-LOCAL_GIT_ENV = "GIT_REMOTE_REPOS"
+gLocalGitEnv = "GIT_REMOTE_REPOS"
 
 # Name of project. Remote git folder name
-PROJECT_NAME = "Tesasa"
+gProjectName = "DefaultProject"
 
 # Set this to false if you set no key phrase for your ssh key
 USE_KEYPHRASE = True
 
 # Keyphrase of ssh key.
-gSshKeyPhrase = ""
+gSshKeyPhrase = ''
 
 def ShellSshProcess(host):
     # Use shell = true because we need powershell comandlets, not ssh. You hae to be ensure that local and remote uese powershell as default shell
@@ -33,7 +33,7 @@ def PowershellProcess():
     return subprocess.Popen(["powershell"], stdin=subprocess.PIPE, stdout = subprocess.PIPE, universal_newlines = True, bufsize = 0, shell = False)
 
 def GetGitFolderPath(process):
-    process.stdin.write(f"$env = Get-ChildItem -Path Env:\{REMOTE_GIT_ENV}\n")
+    process.stdin.write(f"$env = Get-ChildItem -Path Env:\{gRemoteGitEnv}\n")
     process.stdin.write("$value = $env.value\n")
     """ process.stdin.write("$value\n")
     buffer = process.stdout.readlines()
@@ -43,8 +43,8 @@ def ChangeDirToGit(process):
     process.stdin.write("cd \"$value\"\n")
 
 def CreateProjectFolder(process):
-    process.stdin.write(f"New-Item -Path . -Name \"{PROJECT_NAME}.git\" -ItemType \"directory\"\n")
-    process.stdin.write(f"$path = \"$value\" + \"\{PROJECT_NAME}.git\"\n")
+    process.stdin.write(f"New-Item -Path . -Name \"{gProjectName}.git\" -ItemType \"directory\"\n")
+    process.stdin.write(f"$path = \"$value\" + \"\{gProjectName}.git\"\n")
     process.stdin.write("cd $path\n")
 
 def CreateBareRepository(process):
@@ -54,7 +54,7 @@ def Exit(process):
     process.stdin.write("exit\n")
 
 def GetLocalGitPath(process):
-    process.stdin.write(f"$env = Get-ChildItem -Path Env:\{LOCAL_GIT_ENV}\n")
+    process.stdin.write(f"$env = Get-ChildItem -Path Env:\{gLocalGitEnv}\n")
     process.stdin.write("$value = $env.value\n")
 
 def ChangeDirToLocalGit(process):
@@ -62,7 +62,7 @@ def ChangeDirToLocalGit(process):
 
 def CloneRemoteRepo(process):
     #TODO: have to get git remote path from shell to paste there "D:\git\"
-    process.stdin.write(f"git clone \"{HOST}:D:\git\{PROJECT_NAME}.git\"\n")
+    process.stdin.write(f"git clone \"{gHostAddress}:D:\git\{gProjectName}.git\"\n")
     if USE_KEYPHRASE:
         #Sleep for password input appears
         time.sleep(5)
@@ -78,7 +78,7 @@ def PrintBuffer(process, header):
         print (result)
 
 def CreateRemoteRepository():
-    ssh = ShellSshProcess(HOST)
+    ssh = ShellSshProcess(gHostAddress)
     GetGitFolderPath(ssh)
     ChangeDirToGit(ssh)
     CreateProjectFolder(ssh)
@@ -99,7 +99,17 @@ def CreateCloneRepository():
 def GetPassFromUser():
     gSshKeyPhrase = getpass()
 
-if USE_KEYPHRASE: GetPassFromUser()
-CreateRemoteRepository()
-CreateCloneRepository()
+if __name__ == '__main__':
+
+    gProjectName = sys.argv[0]
+    if gProjectName is None or gProjectName == '':
+        print("Project name is empty!")
+        sys.exit(0)
+    if USE_KEYPHRASE: 
+        gSshKeyPhrase = sys.argv[1]
+        if gSshKeyPhrase is None or gSshKeyPhrase == '':
+            gSshKeyPhrase = GetPassFromUser()
+
+    CreateRemoteRepository()
+    CreateCloneRepository()
 
